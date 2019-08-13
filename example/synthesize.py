fb43b209bc5ad49a5bb40a61a4086fe7b5833263
Ryan Jacobs <ryan@rmj.us>
2019-08-13 03:46:06 -0700

    fatal

--- example/synthesize.py
+++ example/synthesize.py
@@ -6,12 +6,17 @@ import sys
 import json
 import base64
 
-if len(sys.argv) != 2:
+# Usage information
+if len(sys.argv) <= 2:
     print(f"Usage: {sys.argv[0]} <top_module_name> <source.v> [pinmap.pcf] [source.v ...]\n")
     print("Send a set of Verilog source files to a remote IceStorm synthesis toolchain flow.")
     print("The resulting bitstream is saved as 'bitstream.bin'\n")
     print(f"Example: {sys.argv[0]} fpga_top blinky.v")
     sys.exit()
+
+# Parse arguments
+top_module = sys.argv[1]
+files = sys.argv[2:]
 
 # Create WebSocket Connection
 from websocket import create_connection
@@ -19,21 +24,30 @@ ws = create_connection(SERVER)
 
 # Send Synthesis Request
 request = {"type": "request_synthesis"}
-ws.send(request)
+ws.send(json.dumps(request))
 
 # Log toolchain stdout/stderr to console
 while True:
     res  = ws.recv()
     data = json.loads(res)
 
+    # Bitstream received! Close the WebSocket and break
     if data["type"] == "bitstream":
+        ws.close()
         break
+
+    # Uh oh... error. Print the message and quit.
+    elif data["type"] == "error":
+        print("FATAL ERROR:", data["msg"])
+        ws.close()
+        sys.exit()
+
+    # Log message... print it.
     elif data["type"] == "log":
         strings = list(data["arguments"].values())
         print(" ".join(strings))
 
-# Close WebSocket and save the bitstream
-ws.close()
+# Save the bitstream
 bitstream = base64.b64decode(data["bitstream"])
 with open("bitstream.bin", "wb") as file:
     file.write(bitstream)
