3ad0a8df2fc5b857df4b864e99f550bb84535d6a
Ryan Jacobs <ryan@rmj.us>
2019-08-13 03:31:01 -0700

    synth example

---
+++ example/synthesize.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import json
+import base64
+from websocket import create_connection
+ws = create_connection("ws://localhost:2019")
+
+# Send Synthesis Request
+example = '{"type": "request_synthesis", "files": [{"name": "uh.v", "body": "module top(output wire led, input wire button); assign led = button; endmodule"}, {"name": "pinmap.pcf", "body": "set_io led 31\\n set_io button 42"}], "top_module": "top"}'
+ws.send(example)
+
+# Log toolchain stdout/stderr to console
+while True:
+    res  = ws.recv()
+    data = json.loads(res)
+
+    if data["type"] == "bitstream":
+        break
+    elif data["type"] == "log":
+        strings = list(data["arguments"].values())
+        print(" ".join(strings))
+
+# Close WebSocket and save the bitstream
+ws.close()
+bitstream = base64.b64decode(data["bitstream"])
+with open("bitstream.bin", "wb") as file:
+    file.write(bitstream)
