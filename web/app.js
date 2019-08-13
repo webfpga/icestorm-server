31919a7c773bace219cf2a3e49a121f039f6fa26
Ryan Jacobs <ryan@rmj.us>
2019-08-13 01:43:24 -0700

    wss

--- web/app.js
+++ web/app.js
@@ -1,1 +1,12 @@
 #!/usr/bin/env node
+
+const WebSocket = require("ws");
+const wss = new WebSocket.Server({port: 2019});
+
+wss.on("connection", function connection(ws) {
+  ws.on("message", function incoming(message) {
+    console.log("received: %s", message);
+  });
+
+  ws.send("something");
+});
