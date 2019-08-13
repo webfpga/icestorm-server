091e9f0d8f8a33026617183ad0b74dc11c43bded
Ryan Jacobs <ryan@rmj.us>
2019-08-13 02:17:18 -0700

    wip

--- web/app.js
+++ web/app.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
 
+// Node.js Standard Library
+const os   = require("os");
+const fs   = require("fs");
+const path = require("path");
+
+// NPM Dependencies
 const WebSocket = require("ws");
 
 // Create WebSocket Server
@@ -42,5 +48,46 @@ function on_msg(ws) {
 // Synthesis Request
 function synthesize(ws, data) {
   console.log("Recieved Synthesis Request...");
-  console.log("Generating temp directory");
+
+  // Generate a temp directory
+  console.log("Generating temp directory...");
+  const tmpdir = fs.mkdtempSync(path.join(os.tmpdir(), "icestorm-server_"));
+  console.log(tmpdir);
+
+  try {
+    // Save the files the the temp directory, then synthesize the HDL.
+    console.log("Saving source files to temp directory...");
+    for (let file of data.files) {
+      console.log(file.name);
+      fs.writeFileSync(`${tmpdir}/${file.name}`, file.body);
+    }
+
+    // Run the IceStorm toolchain flow
+    console.log("Running IceStorm flow with Make...");
+    run_icestorm_make(tmpdir, data.top_module);
+  } catch (e) {
+    console.error(e);
+    console.error("error: unable to synthesize");
+  } finally {
+    // Purge temp directory
+    /*rmdir(tmpdir);*/
+  }
+}
+
+function run_icestorm_make(dir, top_module) {
+}
+
+// https://stackoverflow.com/a/32197381
+function rmdir(path) {
+  if (fs.existsSync(path)) {
+    fs.readdirSync(path).forEach(function(file, index) {
+      var curPath = path + "/" + file;
+      if (fs.lstatSync(curPath).isDirectory()) { // recurse
+        deleteFolderRecursive(curPath);
+      } else { // delete file
+        fs.unlinkSync(curPath);
+      }
+    });
+    fs.rmdirSync(path);
+  }
 }
