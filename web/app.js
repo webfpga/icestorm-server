91b87cb1fdd80764e80897ddbe88dab78e046364
Ryan Jacobs <ryan@rmj.us>
2019-08-13 01:58:26 -0700

    wip

--- web/app.js
+++ web/app.js
@@ -1,12 +1,46 @@
 #!/usr/bin/env node
 
 const WebSocket = require("ws");
-const wss = new WebSocket.Server({port: 2019});
 
-wss.on("connection", function connection(ws) {
-  ws.on("message", function incoming(message) {
-    console.log("received: %s", message);
-  });
+// Create WebSocket Server
+const port = process.env.port || 2019;
+const wss  = new WebSocket.Server({port},
+  () => console.log("listening on ws://0.0.0.0:"+port));
 
-  ws.send("something");
+// Create WebSocket Message Handler
+wss.on("connection", ws => {
+  ws.on("message", on_msg(ws));
 });
+
+function on_msg(ws) {
+  return msg => {
+    // Parse JSON WebSocket Message
+    let data;
+    try {
+      data = JSON.parse(msg);
+      console.log("recv", data);
+    } catch (e) {
+      console.error("error: unable to parse JSON");
+      data = {type: "JSON_Error"};
+    }
+
+    // Route Requests
+    switch (data.type) {
+      case "request_synthesis":
+        synthesize(ws, data);
+        break;
+
+      default:
+        console.error("error: unrecognized data type:", data.type);
+        break;
+    }
+
+    console.log("----");
+  };
+}
+
+// Synthesis Request
+function synthesize(ws, data) {
+  console.log("Recieved Synthesis Request...");
+  console.log("Generating temp directory");
+}
