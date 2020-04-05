77024fd9464e36a5ea0186608643c47964c9d8ff
Ryan Jacobs <ryan@rmj.us>
2020-04-05 11:32:23 -0700

    ubuntu:20

--- build-docker.sh
+++ build-docker.sh
@@ -3,7 +3,8 @@
 cache=false
 ip addr | grep 10.0.10.0/24 && cache=true
 
-for base in ubuntu:18.04 ubuntu:20.04; do
+#for base in ubuntu:18.04 ubuntu:20.04; do
+for base in ubuntu:20.04; do
     version=0.0.3-$(tr ':' '-' <<< $base)
     docker build\
         --build-arg BASE_IMAGE=$base\
