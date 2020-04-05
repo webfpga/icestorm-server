8b89266ef2afe3843c80d1be22d94a04d3bd072a
Ryan Jacobs <ryan@rmj.us>
2020-04-05 11:18:22 -0700

    hide latest

--- build-docker.sh
+++ build-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-for base in ubuntu:18.04 ubuntu:18.10 ubuntu:19.04 ubuntu:19.10 ubuntu:20.04; do
+for base in ubuntu:18.04 ubuntu:20.04; do
     version=0.0.3-$(tr ':' '-' <<< $base)
     docker build --build-arg BASE_IMAGE=$base --tag webfpga/icestorm-server:$version .
-    docker tag webfpga/icestorm-server:$version webfpga/icestorm-server:latest
+    #docker tag webfpga/icestorm-server:$version webfpga/icestorm-server:latest
 done
