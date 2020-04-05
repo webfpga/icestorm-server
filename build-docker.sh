e2023ca2cd3334bc38a574c4dcf9a851b073c89c
Ryan Jacobs <ryan@rmj.us>
2020-04-05 11:30:31 -0700

    use cache on dukelana net

--- build-docker.sh
+++ build-docker.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+cache=false
+ip addr | grep 10.0.10.0/24 && cache=true
+
 for base in ubuntu:18.04 ubuntu:20.04; do
     version=0.0.3-$(tr ':' '-' <<< $base)
-    docker build --build-arg BASE_IMAGE=$base --tag webfpga/icestorm-server:$version .
+    docker build\
+        --build-arg BASE_IMAGE=$base\
+        --build-arg USE_CACHE=cache\
+        --tag webfpga/icestorm-server:$version .
     #docker tag webfpga/icestorm-server:$version webfpga/icestorm-server:latest
 done
