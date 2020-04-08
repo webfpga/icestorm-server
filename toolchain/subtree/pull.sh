cb2fe94757edd803743deb089ef48db44077fe83
Ryan Jacobs <ryan@rmj.us>
2020-04-07 23:54:08 -0700

    fix pull script

--- toolchain/subtree/pull.sh
+++ toolchain/subtree/pull.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd ..
-git subtree -P subtree/icestorm pull https://github.com/cliffordwolf/icestorm master --squash
-git subtree -P subtree/yosys    pull https://github.com/YosysHQ/yosys         master --squash
-git subtree -P subtree/nextpnr  pull https://github.com/YosysHQ/nextpnr       master --squash
+cd $(git rev-parse --show-toplevel)
+git subtree -P toolchain/subtree/icestorm pull https://github.com/cliffordwolf/icestorm master --squash
+git subtree -P toolchain/subtree/yosys    pull https://github.com/YosysHQ/yosys         master --squash
+git subtree -P toolchain/subtree/nextpnr  pull https://github.com/YosysHQ/nextpnr       master --squash
