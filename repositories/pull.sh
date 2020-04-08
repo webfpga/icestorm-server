e79b2673ed500423a5e1e9fcf72a36c5fbd47638
Ryan Jacobs <ryan@rmj.us>
2020-04-07 18:32:51 -0700

    typo in repo name

--- repositories/pull.sh
+++ repositories/pull.sh
@@ -2,4 +2,4 @@
 cd ..
 git subtree -P repositories/icestorm pull https://github.com/cliffordwolf/icestorm master --squash
 git subtree -P repositories/yosys    pull https://github.com/YosysHQ/yosys         master --squash
-git subtree -P repositories/nextpnr  pull https://github.com/YosysHQ/nextpn        master --squash
+git subtree -P repositories/nextpnr  pull https://github.com/YosysHQ/nextpnr       master --squash
