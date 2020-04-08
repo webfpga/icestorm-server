11d78c7eac2c7ae4ace2aa4d27720438cbde8bd1
Ryan Jacobs <ryan@rmj.us>
2020-04-07 23:56:15 -0700

    rm add

--- toolchain/subtree/add.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd $(git rev-parse --show-toplevel)
-git subtree -P toolchain/subtree/icestorm add https://github.com/cliffordwolf/icestorm master --squash
-git subtree -P toolchain/subtree/yosys    add https://github.com/YosysHQ/yosys         master --squash
-git subtree -P toolchain/subtree/nextpnr  add https://github.com/YosysHQ/nextpnr       master --squash
