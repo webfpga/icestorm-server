3c4d712db1193a3ef49a2317ad43caf32c23dc8e
Ryan Jacobs <ryan@rmj.us>
2020-04-08 03:42:22 -0700

    switch to git sha ref versions

--- toolchain/build-docker.sh
+++ toolchain/build-docker.sh
@@ -3,7 +3,7 @@
 cache=false
 ip addr | grep 10.0.10.0/24 && cache=true
 
-version=0.2.2
+version=0.2.2-$(git rev-parse --short HEAD)
 name="webfpga/icestorm-base"
 
 docker build --tag $name:$version .
