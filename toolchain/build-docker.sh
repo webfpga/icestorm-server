dad1c19948b6e71cb4597256e22a60f57463c92b
Ryan Jacobs <ryan@rmj.us>
2020-04-07 23:50:37 -0700

    mv subtree

---
+++ toolchain/build-docker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cache=false
+ip addr | grep 10.0.10.0/24 && cache=true
+
+version=0.1.2
+docker build --tag webfpga/icestorm-server:$version .
+docker tag webfpga/icestorm-server:$version webfpga/icestorm-server:latest
