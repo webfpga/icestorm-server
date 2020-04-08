de47a0b88dd0d4e219bc5168426de53ff832d163
Ryan Jacobs <ryan@rmj.us>
2020-04-07 23:51:39 -0700

    toolchain build

--- toolchain/build-docker.sh
+++ toolchain/build-docker.sh
@@ -3,6 +3,8 @@
 cache=false
 ip addr | grep 10.0.10.0/24 && cache=true
 
-version=0.1.2
-docker build --tag webfpga/icestorm-server:$version .
-docker tag webfpga/icestorm-server:$version webfpga/icestorm-server:latest
+version=0.2.0
+name="webfpga/icestorm"
+
+docker build --tag $name:$version .
+docker tag $name:$version $name:latest
