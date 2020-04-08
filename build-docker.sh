1d4b00f7c3dac2f3d5278af56f9e276ef9a8d287
Ryan Jacobs <ryan@rmj.us>
2020-04-07 19:01:52 -0700

    rm base

--- build-docker.sh
+++ build-docker.sh
@@ -4,6 +4,6 @@ cache=false
 ip addr | grep 10.0.10.0/24 && cache=true
 
 #for base in ubuntu:18.04 ubuntu:20.04; do
-version=0.0.5-$(tr ':' '-' <<< $base)
+version=0.0.5
 docker build --tag webfpga/icestorm-server:$version .
 docker tag webfpga/icestorm-server:$version webfpga/icestorm-server:latest
